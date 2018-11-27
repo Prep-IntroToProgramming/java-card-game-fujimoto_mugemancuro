37bdd43ece77e7329e4f8444505249c7f0b17915
Peter Mugemancuro <19mugemancurop@flintridgeprep.org>
2018-11-27 09:33:57 -0800

    New Player class created, TestDeck sent to sunken place.

---
+++ Player.java
@@ -0,0 +1,21 @@
+import java.util.*;
+/**
+ * Write a description of class Player here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Player
+{
+    // instance variables - replace the example below with your own
+    private String name;
+    private Deck hand;
+
+    public void showHand(){
+    }
+    public void addCard(Card c){
+    }
+    public int calcHand(){
+    }
+    
+}
