bc9067c41495042d4c0736c63268aeb24d8f8c60
Peter Mugemancuro <19mugemancurop@flintridgeprep.org>
2018-11-27 09:44:14 -0800

    Encountering expected difficulties.

--- Player.java
+++ Player.java
@@ -1,21 +1,21 @@
 import java.util.*;
-/**
- * Write a description of class Player here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Player
 {
     // instance variables - replace the example below with your own
     private String name;
     private Deck hand;
-
+    ArrayList<Card>playerCards = new ArrayList<Card>();
     public void showHand(){
+
     }
+
     public void addCard(Card c){
+        for (int i; i<1; i++){
+            playerCards.add(cards.get(i));
+        }
     }
+
     public int calcHand(){
     }
-    
+
 }
