a461e0614c2225e520a0cb9ad059069f260998d0
PM <19mugemancurop@flintridgeprep.org>
2018-12-04 12:58:13 -0800

    Changes to game().

--- Game.java
+++ Game.java
@@ -5,6 +5,7 @@ public class Game
    Player newPlayer = new Player();
    Player dealer = new Player();
    public void setUpGame(){
+        newDeck.shuffle();
         newPlayer.addCard(newDeck.draw());
         newPlayer.addCard(newDeck.draw());
         dealer.addCard(newDeck.draw());
@@ -17,9 +18,21 @@ public class Game
        return true;
    }
    public void playTurn(){
+       Scanner startPlay = new Scanner(System.in);
+       int storedVal = startPlay.nextInt();
+       System.out.print ("Would you like to hit? If yes, press 1. If no, press 2.");
+       while (!startPlay.hasNextInt() && storedVal>1 && storedVal<2){
+           System.out.print ("That is not a valid value. Try again.");
+       }
        
    }
    public void main (String[] args){
-   
+       Game newGame = new Game();
+       newGame.setUpGame();
+       if(newGame.hasBlkJk(newGame.newPlayer)){
+           System.out.print("You Win!");
+           return;
+       }
+       //We need to add similar functionality for dealerHand.
    }
 }
