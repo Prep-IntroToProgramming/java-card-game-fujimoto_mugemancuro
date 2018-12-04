69cb25c3a566ad6f9f577279467faf6ea5501545
Alison Fujimoto <19fujimotoa@flintridgeprep.org>
2018-12-03 16:29:55 -0800

    added comments to player and deck class

--- Deck.java
+++ Deck.java
@@ -3,22 +3,26 @@ import java.util.*;
 
 public class Deck
 {
+    //creating a new deck of cards by creating an ArrayList named cards
     ArrayList<Card> cards = new ArrayList<Card>();      
     public Deck(){
+        //assigning a suit to each card
         for (int i=1; i<=4; i++){
+            //assinging a rank to each card and adding the new cards into the ArrayList or deck
             for (int j=1; j<=13; j++){
                 cards.add(new Card(i,j));
             }
         }
     }
-
+    //removing/drawing a card from the top of the deck
     public Card draw() {
         Card cardDrawn = cards.get(0);
         cards.remove(0);
         return cardDrawn;
     }
-
+    //shuffle card method 
     public void shuffle(){
+        //accessing java utility shuffle function
         Collections.shuffle(cards);
     }
 
--- Player.java
+++ Player.java
@@ -1,21 +1,30 @@
 import java.util.*;
 public class Player
 {
+    //declaring a name variable for the player
     private String name;
+    //creating the player's cards using an ArrayList called playerCards
     ArrayList<Card>playerCards = new ArrayList<Card>();
+    //showing the player the rank and suit of their hand
     public void showHand(){
         for (Card testCard:playerCards){
             System.out.print (" "+testCard.rankToString() +" "+"of"+" "+testCard.suitToString());
         }
     }
-
+    //adding card c to the player's hand
     public void addCard(Card c){
         playerCards.add(c);
     }
-
+    //method to calculate the value of the player's hand
     public int calcHand(){
+        //declaring and initializing handVal variable which represents the value of the player's hand
         int handVal = 0;
+        //declaring and initializing acePresent variable which checks if the player has an ace in their hand
         boolean acePresent = false;
+        /* 
+         * if player has an ace, ace takes on a value of 1. if rank of other card 
+         * is >= 10, the ace takes on a value of 11...
+         */
         for (Card card:playerCards){
             if (card.rank == 1){
                 acePresent = true;
@@ -26,6 +35,10 @@ public class Player
                     handVal = handVal + card.rank;
             }
         }
+        /*
+         * if player has an ace and their hand value is less than or equal to 11,
+         * ace will assume a value of 11
+         */ 
         if (acePresent==true && handVal<=11){
                 handVal = handVal + 10;
         }
